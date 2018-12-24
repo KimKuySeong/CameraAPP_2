6920d22bf0f68149dca72662fd5ba08971aee485
KimKyuSong <rbtjd5087@naver.com>
2018-12-24 11:55:14 +0900

    카메라 구동 안함 알러트창 안뜸
    카메라 앱 만들기 실패~

--- Project2/ViewController.m
+++ Project2/ViewController.m
@@ -7,8 +7,25 @@
 //
 
 #import "ViewController.h"
+#import "GPUImage.h"
+#import <AssetsLibrary/ALAssetsLibrary.h>
 
-@interface ViewController ()
+
+@class GPUImageStillCamera;
+
+@interface ViewController () {
+    
+    __weak IBOutlet GPUImageView *m_caremaView;
+    
+    GPUImageStillCamera *m_stillCamera;
+    
+    GPUImageiOSBlurFilter *m_blurFilter;
+    
+    GPUImageCropFilter *m_cropFilter;
+    
+}
+- (IBAction)ChangeLevel:(id)sender;
+- (IBAction)shotBtnAction:(id)sender;
 
 @end
 
@@ -16,8 +33,71 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+-(void)initCamera{
+    // 카메라 생성 및 세팅
+    m_stillCamera = [[GPUImageStillCamera alloc] init];
+    m_stillCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    
+    // 필터 생성 및 카메라에 필터 추가
+    m_blurFilter = [[GPUImageiOSBlurFilter alloc] init];
+    [m_stillCamera addTarget:m_blurFilter];
+    
+    // 카메라 뷰에 필터 추가
+    [m_blurFilter addTarget:m_caremaView];
+    
+    
+    // view 크기에 맞게 꽉차게
+    m_caremaView.fillMode = kGPUImageFillModePreserveAspectRatioAndFill;
+    
+    // 카메라 캡쳐 시작
+    [m_stillCamera startCameraCapture];
+}
+- (IBAction)ChangeLevel:(id)sender {
+    
+    UISlider *slider =(UISlider*)sender;
+    
+    if (slider.tag ==0)
+    {
+        m_blurFilter.blurRadiusInPixels =slider.value;
+    }
+    else if (slider.tag ==1)
+    {
+        m_blurFilter.saturation =slider.value;
+        
+    }
+    else if (slider.tag ==2)
+    {
+        m_blurFilter.downsampling =slider.value;
+    }
+    else
+    {
+        m_blurFilter.rangeReductionFactor =slider.value;
+    }
 }
 
+- (IBAction)shotBtnAction:(id)sender {
+    [m_stillCamera capturePhotoAsImageProcessedUpToFilter:m_blurFilter withCompletionHandler:^(UIImage *processedImage, NSError *error)
+     {
+         
+         [m_stillCamera pauseCameraCapture];
+         
+         UIImageWriteToSavedPhotosAlbum(processedImage,self,
+                                        @selector(image:didFinishSavingWithError:contextInfo:),
+                                        nil);
+     }];
+    
+}
 
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo: (void *)contextInfo
+{
+    if (error != nil)
+    {
+        NSLog(@"Image Can not be saved");
+    }
+    else
+    {
+        NSLog(@"Successfully saved Image");
+    }
+}
 @end
